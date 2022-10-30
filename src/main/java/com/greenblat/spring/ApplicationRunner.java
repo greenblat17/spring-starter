4fe298525c1e9fdddcf5ef077183befb5c098079
Alex Zhuravlev <leftemail17@mail.ru>
2022-10-30 18:46:10 +0300

    xml-based config

--- src/main/java/com/greenblat/spring/ApplicationRunner.java
+++ src/main/java/com/greenblat/spring/ApplicationRunner.java
@@ -5,16 +5,14 @@ import com.greenblat.spring.database.repository.CompanyRepository;
 import com.greenblat.spring.database.repository.UserRepository;
 import com.greenblat.spring.ioc.Container;
 import com.greenblat.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var container = new Container();
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-        var userService = container.getClass(UserService.class);
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        // clazz -> String -> Map<String, Object>
+        var connectionPool = context.getBean("pool2", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
