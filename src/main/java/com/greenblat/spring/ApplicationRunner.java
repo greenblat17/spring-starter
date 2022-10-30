4074f9acd2df26840fdac8793cfb672cb0306639
Alex Zhuravlev <leftemail17@mail.ru>
2022-10-30 19:24:46 +0300

    constructor injection

--- src/main/java/com/greenblat/spring/ApplicationRunner.java
+++ src/main/java/com/greenblat/spring/ApplicationRunner.java
@@ -12,7 +12,7 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
         // clazz -> String -> Map<String, Object>
-        var connectionPool = context.getBean("pool2", ConnectionPool.class);
+        var connectionPool = context.getBean("pool1", ConnectionPool.class);
         System.out.println(connectionPool);
     }
 }
