559282f851f7c396149c017d0c46f0bfc1120c6d
Alex Zhuravlev <leftemail17@mail.ru>
2022-11-01 17:15:43 +0300

    classpath scanning

--- src/main/java/com/greenblat/spring/database/repository/UserRepository.java
+++ src/main/java/com/greenblat/spring/database/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.greenblat.spring.database.repository;
 
 import com.greenblat.spring.database.pool.ConnectionPool;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UserRepository {
 
     private final ConnectionPool connectionPool;
--- src/main/java/com/greenblat/spring/service/CompanyService.java
+++ src/main/java/com/greenblat/spring/service/CompanyService.java
@@ -1,11 +1,13 @@
 package com.greenblat.spring.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CompanyService {
 
-    private UserService userService;
+    private final UserService userService;
 
-    // зацикливание
-    public void setUserService(UserService userService) {
+    public CompanyService(UserService userService) {
         this.userService = userService;
     }
 }
