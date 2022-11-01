20dd6b974d928d95db5c1abf3643f526ed12d244
Alex Zhuravlev <leftemail17@mail.ru>
2022-11-01 18:02:36 +0300

    @Scope

--- src/main/java/com/greenblat/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/greenblat/spring/database/repository/CompanyRepository.java
@@ -6,6 +6,8 @@ import com.greenblat.spring.database.entity.Company;
 import com.greenblat.spring.database.pool.ConnectionPool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
@@ -13,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
