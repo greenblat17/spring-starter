b8fe8663b1b999a3aa54b66c983efeee1b4d385b
Alex Zhuravlev <leftemail17@mail.ru>
2022-11-05 08:17:28 +0300

    java based config

---
+++ src/main/java/com/greenblat/spring/config/ApplicationConfiguration.java
@@ -0,0 +1,23 @@
+package com.greenblat.spring.config;
+
+import com.greenblat.spring.database.repository.CrudRepository;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.greenblat.spring",
+        useDefaultFilters = false,
+        includeFilters = {
+                @Filter(type = FilterType.ANNOTATION, value = Component.class),
+                @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
+                @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
+        })
+public class ApplicationConfiguration {
+
+
+}
